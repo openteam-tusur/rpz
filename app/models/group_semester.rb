1aef9ddfb4d2c921b329b97ed3003d128720f154
Dmitriy Baraksanov <bdn@openteam.ru>
2013-04-25 17:05:44 +0700

    вывел данные РПЗ

--- app/models/group_semester.rb
+++ app/models/group_semester.rb
@@ -5,11 +5,29 @@ class GroupSemester < ActiveRecord::Base
   belongs_to :ends_on_week, :class_name => "Week"
   belongs_to :semester
   has_many :educations, dependent: :destroy
+  has_many :trainings, through: :educations
   scope :autumn, -> { joins(:semester).where('semesters.title' => :autumn) }
+  scope :spring, -> { joins(:semester).where('semesters.title' => :spring) }
 
   def number
     semester_number = group.course*2 -1
     semester_number += 1 if semester.title.spring?
     semester_number
+  end
+
+  def weeks_count
+    weeks.count
+  end
+
+  def weeks
+    semester.year.weeks.where(:number => [starts_on_week.number..ends_on_week.number])
+  end
+
+  def css_class_for(week)
+    'breaks' if week.eql?(breaks_on_week)
+  end
+
+  def summ_planned_loading
+    trainings.sum(&:planned_loading)
   end
 end
