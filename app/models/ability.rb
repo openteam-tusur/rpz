cb2f6c347f49c78b19ff761056fbebee13422e5c
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-04-26 16:12:37 +0700

    Manager ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
   def initialize(user)
     return unless user
 
-    can :manage, :application
-    can :manage, [User, Permission, Year] if user.manager?
+    can :manage, :application if user.permissions.any?
+    can :manage, :all if user.manager?
   end
 end
