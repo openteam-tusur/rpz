d9ac6d5762a151e66ecfcd73e71fa98d64a21d8a
tab <tukhfatullin@gmail.com>
2013-04-26 16:17:16 +0700

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.find_or_initialize_by_email('mail@openteam.ru').tap do |user|
+  user.save(validate: false)
+  user.permissions << Permission.new(role: :manager)
+end
